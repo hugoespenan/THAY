d1f7fc89d07f2002326c7aa8c7972d0fdc575d39
Y4CNN <boualiyacine15@gmail.com>
2023-02-27 14:26:07 +0100

    Suppression de qlq images et ajout de nouvelle image et d'un logo au millieu de la page index.php

---
+++ volley/admin.php
@@ -0,0 +1,113 @@
+<?php
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Sportz &mdash; Colorlib Sports Team Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<div class="site-wrap">
+
+
+
+    <header class="site-navbar absolute transparent" role="banner">
+        <nav class="site-navigation position-relative text-right bg-black text-md-right" role="navigation">
+            <div class="container position-relative">
+                <div class="site-logo">
+
+                </div>
+
+                <div class="d-inline-block d-md-none ml-md-0 mr-auto py-3">
+                    <a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a>
+                </div>
+
+                <ul class="site-menu js-clone-nav d-none d-md-block">
+
+                    <li><a href="login.html">Se deonnecter</a></li>
+
+
+                </ul>
+            </div>
+            <div class="admin" style="text-align: center ; color: white; font-size: 26px;">
+                <p>Interface Administrateur</p>
+            </div>
+        </nav>
+    </header>
+
+    <body>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col"></th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+        </tr>
+        </tbody>
+    </table>
+    </body>
+
+</div>
+
+
+
+
+
+
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-migrate-3.0.1.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.stellar.min.js"></script>
+<script src="js/jquery.countdown.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/aos.js"></script>
+
+<script src="js/main.js"></script>
+
+</body>
+</html>
